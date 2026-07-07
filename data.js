db9d28f50a4321a803ffdb2b30c40374a6380dbf
chathurani12 <dilrukshichathurani1999@gmail.com>
2026-07-07 19:57:44 +0530

    Create Node API wireframe

---
+++ data.js
@@ -0,0 +1,6 @@
+const users = [
+    { id: 1, name: "Deeghayu Adhikari", role: "Admin" },
+    { id: 2, name: "chathurani dilrukshi", role: "User" }
+];
+
+module.exports = users;
---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const users = require("./data");
+
+const app = express();
+
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+    res.send("Hello! My Node.js server is running!");
+});
+
+app.get("/api/users", (req, res) => {
+    res.json(users);
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is listening on http://localhost:${PORT}`);
+});
